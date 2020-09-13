16b52b9bbccee47d1a5d3728fbb064b84c2f34d0
PabloCorrea99 <pcorream2@eafit.edu.co>
2020-09-13 11:29:28 -0500

    Cambio navbar

--- forotech/resources/views/layouts/master.blade.php
+++ forotech/resources/views/layouts/master.blade.php
@@ -54,35 +54,24 @@
   <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo mr-auto"><a href="index.html">Rapid</a></h1>
+      <h1 class="logo mr-auto"><a href="index.html">Foro Techsolver</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav class="main-nav d-none d-lg-block">
         <ul>
-          <li class="active"><a href="index.html">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li class="drop-down"><a href="">Drop Down</a>
+          <li class="active"><a href="index.html">Inicio</a></li>
+          <li><a href="#about">Sobre Nosotros</a></li>
+          <li><a href="#services">Noticias</a></li>
+          <li><a href="#portfolio">Foro</a></li>
+          <li><a href="#team">Productos</a></li>
+          <li class="drop-down"><a href="">Perfil</a>
             <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="drop-down"><a href="#">Drop Down 2</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-              <li><a href="#">Drop Down 5</a></li>
+              <li><a href="#">Iniciar Sesión</a></li>
+              <li><a href="#">Crear Cuenta</a></li>
             </ul>
           </li>
-          <li><a href="#footer">Contact Us</a></li>
+          <li><a href="#footer">Contacto</a></li>
         </ul>
       </nav><!-- .main-nav-->
 
