7c007258dc810186b09b6e4ee30c031b89c66fc4
PabloCorrea99 <pcorream2@eafit.edu.co>
2020-11-08 13:50:59 -0500

    Lang en español listo

---
+++ forotech/resources/lang/es/messages.php
@@ -0,0 +1,144 @@
+<?php 
+
+return [
+    //NavBar
+    "Title"=>"Foro Techsolver",
+    "Home"=>"Incio",
+    "Forum"=>"Foro",
+    "Products"=>"Productos",
+    "Team"=>"Equipo",
+    "Account"=>"Cuenta",
+    "Login"=>"Iniciar Sesion",
+    "Register"=>"Registrarse",
+    "Logout"=>"Salir",
+    "Contact"=>"Contacto",
+
+    //Footer
+    "FooterInfo"=>"Foro Techsolver",
+    "FooterText"=>"Esta página esta dirigida a todas las personas interesadas tanto en conocer, como en opinar sobre temas de tecnología (Programación, Videojuegos, Hardware ...). Te invitamos a que disfrutes bastante y respetes la comunidad.",
+    "FooterLinks"=>"Links Utiles",
+    "FooterAbout"=>"Sobre Nosotros",
+    "FooterContact"=>"Contactanos",
+
+    //INDEX.blade
+    //About Section
+    "AbouQuestion"=>"¿De donde sale?",
+    "AboutTextOne"=>"El Foro Techsolver es un foro hispanohablante enfocado totalmente a la tecnología (programación, videojuegos, hardware…), en donde las personas podrán compartir dudas, noticias, conocimiento u opinión, sobre todo lo relacionado a tecnología. ",
+    "AboutTextTwo"=>"Con el foro se pretende aportar al crecimiento de la cultura tecnológica en toda Latinoamérica, puesto que se considera que aun es bastante pobre a comparación con el resto del mundo.",
+    "AboutListOne"=>"Iniciativa Colombiana.",
+    "AboutListTwo"=>"Queremos que cada día crezca mas la cultura tecnologica.",
+    "AboutListThree"=>"Vamos a ser un referente en el mundo tecnologico hispanohablante.",
+
+    //Service Section
+    "SectionTitle"=>"Temas Que Puedes Encontrar",
+    "SectionSubtitle"=>"En este foro puedes hablar de cualquier tema relacionado con tecnología, pero estos son los principales.",
+    "SectionTopicOne"=>"Videojuegos",
+    "SectionTopicOneText"=>"Todo lo relacionado con esto que tanto nos gusta, ultimas noticias de lo que pasa día a día.",
+    "SectionTopicTwo"=>"Programación",
+    "SectionTopicTwoText"=>"Noticias sobre lo nuevo que va pasando en este mundo o también publicaciones para mostar código o resolver dudas.",
+    "SectionTopicThree"=>"Algoritmo",
+    "SectionTopicThreeText"=>"Todo lo que necesitas en temas de algoritmos con los que podras solucionar las problemticas del mundo real.",
+    "SectionTopicFour"=>"Computadores",
+    "SectionTopicFourText"=>"Este vasto que va desde los gamers mas casuales hasta los entuciastas que crean PCs de máximo rendimiento.",
+    "SectionTopicFive"=>"Consolas",
+    "SectionTopicFiveText"=>"Lo que todo el mundo conoce y todo el mundo juega, una rivalidad eterna por áca.",
+    "SectionTopicSix"=>"Actualidad",
+    "SectionTopicSixText"=>"Todas las noticias en general que tengan algún componente tecnologico.",
+
+    //Create Section
+    "CreateTitle"=>"Crea Tu Cuenta Hoy!",
+    "CreateText"=>"Para disfrutar al maximo de nuestra página, es vital que tengas una cuenta. Por eso, te invitamos a crearla hoy mismo, ES MUY FACIL!",
+    "CreateButton"=>"CREAR CUENTA!",
+    
+    //Features Section
+    "FeaturesTitleOne"=>"La tecnología es algo que ya no se puede ignorar",
+    "FeaturesTextOne"=>" Vivimos en un mundo en el que ya todo esta conectado, todo es digital e inteligente. Si queremos hacer parte de este mundo tenemos que conocer sobre lo que pasa todos los días, porque la tecnología avanza a toda velocidad.",
+    "FeaturesTextTwo"=>" Cada día sale algo nuevo que reemplaza a lo que salió ayer, tenemos que seguir ese ritmo o seremos obsoletos.",
+    "FeaturesTitleTwo"=>"Aprovecha el mundo digital y todo lo que ofrece",
+    "FeaturesTextThree"=>" ¿Qué seriamos sin celulares, videojuegos, multimedia...? Exacto, nada. Hoy en día tenemos infinidad de componentes tecnologicos que hacen mucho mas facil nuestra vida.",
+    "FeaturesTextFour"=>" Por eso, no debemos de caer en el error de decir que la tecnología es mala, ella es una herramienta, nosotros le damos el uso. Entonces te invitamos, usa la tecnología, pero siempre de una buena manera.",
+    
+    //Portofolio Section 
+    "PortofolioTitle"=>"Productos Recomendados",
+
+    //Team Section 
+    "TeamTitle"=>"Nuestro Equipo",
+    "TeamSubtitle"=>"Te queremos presentar al equipo que hizo realidad esta página.",
+    "TeamMemberOne"=>"Pablo Correa",
+    "TeamMemberRoleOne"=>"Arquitecto",
+    "TeamMemberTwo"=>"Miguel Restrepo",
+    "TeamMemberRoleTwo"=>"Desarrollador",
+
+    //Brands Section
+    "BrandTitle"=>"Marcas que recomendamos",
+
+    //FAQ Section
+    "FAQTitle"=>"Preguntas Frecuentes",
+    "FAQSubtitle"=>"Un poquito de las preguntas que mas se ven en el foro",
+    "FAQOne"=>"Necesito un super computador para ser programador? ",
+    "FAQAnswerOne"=>"En realidad no, es mas, si vas a hacer programas básicos (aplicaciones web sencillas o algoritmos de baja complejidad), puedes tener cualquier clase de PC y estarás bien.",
+    "FAQTwo"=>"Una Placa Madre Intel me sirve para un procesador AMD?  ",
+    "FAQAnswerTwo"=>"NO! Cada marca de procesadores tiene sus propias lineas de placas madre y en algunos casos dentro de la misma marca hay diferente Placa Madre para algunos procesadores (Ej: Ryzen Threadripper).",
+    "FAQThree"=>"Necesito ser un genio o genia para saber programar?  ",
+    "FAQAnswerThree"=>"No, para nada.",
+    "FAQFour"=>"Cúal consola es mejor PlayStation o Xbox? ",
+    "FAQAnswerFour"=>"Cualquiera de Nintendo por los exclusivos :)",
+    "FAQFive"=>"Necesito tener una tarjeta gráfica para poder jugar videojuegos? ",
+    "FAQAnswerFive"=>"No necesariamente, desde que tengas un procesador con gráficos integrados ya podes tener un PC, lo que si es que las tarjetas gráficas dedicadas van a brindarte un mejor randimiento que los gráficos integrados.",
+    "FAQSix"=>"Cúal es el mejor lenguaje de programación?  ",
+    "FAQAnswerSix"=>"Todo depende de las necesidades de tu sistema y de las habilidades de los integrantes del equipo de desarrollo.",
+
+
+    //Post->CREATE.blade
+    //Create Post
+    "CreatePostTitle"=>"Crear Publicación",
+    "CreatePostLabelOne"=>"Titulo de la Publicación: ",
+    "CreatePostLabelTwo"=>"Escribe tu Publicación: ",
+    "CreatePostLabelThree"=>"Agrega alguna imagen: ",
+    "CreatePostButton"=>"CREAR",
+
+    //Post->LIST.blade
+    //List Post
+    "ListPostTitle"=>"PUBLICACIONES: ",
+    "ListPostCreate"=>"CREAR PUBLICACIÓN!",
+    "ListPostRead"=>"Leer Publicación",
+
+    //Post->SHOW.blade
+    //Show Post
+    "ShowPostAuthor"=>"Autor: ",    
+    "ShowPostLike"=>"Me Gusta",  
+    "ShowPostDislike"=>"No Me Gusta ",  
+    "ShowPostDelete"=>"Eliminar Publicación",
+    "ShowPostCommentTitle"=>"Deja un Comentario!",
+    "ShowPostCommentSubtitle"=>"Opina sobre la Publicacion que acabas de leer o deja una pregunta si la tienes",  
+    "ShowPostCommentList"=>"COMENTARIOS:",  
+
+    //Product->CREATE.blade
+    //Create Product
+    "CreateProductTitle"=>"CREAR PRODUCTO",
+    "CreateProductLabelOne"=>"Nombre del Producto: ", 
+    "CreateProductLabelTwo"=>"Precio del Producto: ",
+    "CreateProductLabelThree"=>"Link de compra: ",
+    "CreateProductButton"=>"AGREGAR",
+
+    //Product->LIST.blade
+    //List Product
+    "ListProductTitle"=>"PRODUCTOS:",
+    "ListProductOne"=>"Precio: ",
+    "ListProductTwo"=>"Link de Compra: ",
+
+    //Product->SHOW.blade
+    //Show Product
+    "ShowProductOne"=>"Nombre del Producto:",
+    "ShowProductTwo"=>"Precio del Producto:",
+    "ShowProductThree"=>"Link de Compra:",
+    "ShowProductButton"=>"Eliminar Producto",
+
+
+
+    
+
+    
+
+
+]
